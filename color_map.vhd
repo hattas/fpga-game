72a9f78a392df6170f5ef7dbc14489cc36ee6a89
John Hattas <johnhattas@gmail.com>
2019-05-02 22:55:24 -0500

    change color map to select 3 bit rgb colors with switches

--- color_map.vhd
+++ color_map.vhd
@@ -12,69 +12,34 @@ entity color_map is
 end color_map;
 
 architecture arch of color_map is
-	signal vga_rgb, vga_rgb_cyan, vga_rgb_magenta, vga_rgb_bw, vga_rgb_3: std_logic_vector(23 downto 0);
+	signal sw_off, sw_on : std_logic_vector(7 downto 0);
 begin
+	
+	with sw(2) select vga_r <= sw_off when '0', sw_on when '1';
+	with sw(1) select vga_g <= sw_off when '0', sw_on when '1';
+	with sw(0) select vga_b <= sw_off when '0', sw_on when '1';
 
 	process(rgb)
 	begin
 		 case rgb is
-			  when "000" => vga_rgb_cyan <= x"222222"; --black
-			  when "001" => vga_rgb_cyan <= x"003F3F";
-			  when "010" => vga_rgb_cyan <= x"007F7F";
-			  when "011" => vga_rgb_cyan <= x"00BFBF";
-			  when "100" => vga_rgb_cyan <= x"00FFFF";
-			  when "101" => vga_rgb_cyan <= x"55FFFF";
-			  when "110" => vga_rgb_cyan <= x"AAFFFF";
-			  when "111" => vga_rgb_cyan <= x"FFFFFF"; --white
-			  when others => vga_rgb_cyan <= x"FF0000";
-		 end case;
-		 
-		 case rgb is
-			  when "000" => vga_rgb_magenta <= x"222222"; --black
-			  when "001" => vga_rgb_magenta <= x"3F003F";
-			  when "010" => vga_rgb_magenta <= x"7F007F";
-			  when "011" => vga_rgb_magenta <= x"BF00BF";
-			  when "100" => vga_rgb_magenta <= x"FF00FF";
-			  when "101" => vga_rgb_magenta <= x"FF55FF";
-			  when "110" => vga_rgb_magenta <= x"FFAAFF";
-			  when "111" => vga_rgb_magenta <= x"FFFFFF"; --white
-			  when others => vga_rgb_magenta <= x"FF0000";
-		 end case;
-		 
-		 case rgb is
-			  when "000" => vga_rgb_bw <= x"000000"; --black
-			  when "001" => vga_rgb_bw <= x"242424";
-			  when "010" => vga_rgb_bw <= x"484848";
-			  when "011" => vga_rgb_bw <= x"6D6D6D";
-			  when "100" => vga_rgb_bw <= x"919191";
-			  when "101" => vga_rgb_bw <= x"B6B6B6";
-			  when "110" => vga_rgb_bw <= x"DADADA";
-			  when "111" => vga_rgb_bw <= x"FFFFFF"; --white
-			  when others => vga_rgb_bw <= x"FF0000";
-		 end case;
-		 
-		 case rgb is
-			  when "000" => vga_rgb_3 <= x"000000"; --black
-			  when "001" => vga_rgb_3 <= x"0000FF";
-			  when "010" => vga_rgb_3 <= x"00FF00";
-			  when "011" => vga_rgb_3 <= x"00FFFF";
-			  when "100" => vga_rgb_3 <= x"FF0000";
-			  when "101" => vga_rgb_3 <= x"FF00FF";
-			  when "110" => vga_rgb_3 <= x"FFFF00";
-			  when "111" => vga_rgb_3 <= x"FFFFFF"; --white
-			  when others => vga_rgb_3 <= x"FF0000";
+			when "000" =>
+				sw_off <= x"00"; sw_on <= x"00";
+			when "001" =>
+				sw_off <= x"00"; sw_on <= x"3F";
+			when "010" =>
+				sw_off <= x"00"; sw_on <= x"7F";
+			when "011" =>
+				sw_off <= x"00"; sw_on <= x"BF";
+			when "100" =>
+				sw_off <= x"00"; sw_on <= x"FF";
+			when "101" =>
+				sw_off <= x"55"; sw_on <= x"FF";
+			when "110" =>
+				sw_off <= x"AA"; sw_on <= x"FF";
+			when "111" =>
+				sw_off <= x"FF"; sw_on <= x"FF";
+			when others =>
+				sw_off <= x"00"; sw_on <= x"00";
 		 end case;
 	end process;
-	
-	
-	with sw(1 downto 0) select vga_rgb <=
-		vga_rgb_cyan 	when "00",
-		vga_rgb_magenta when "01",
-		vga_rgb_bw   	when "10",
-		vga_rgb_3 		when "11";
-	
-	
-	vga_r <= vga_rgb(23 downto 16);
-	vga_g <= vga_rgb(15 downto 8);
-	vga_b <= vga_rgb(7 downto 0);
 end arch;
